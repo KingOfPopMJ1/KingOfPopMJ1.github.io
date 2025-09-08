76abe681b0d61745e48fd5fcf823ec47f40a37ba
Kento Nobuhara <kento.nobuhara@education.nsw.gov.au>
2025-09-08 20:46:24 +1000

    compile script

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,8 +1,12 @@
 var ssi = require("ssi");
  
-var inputDirectory = "inputFiles";
-var outputDirectory = "../";
-var matcher = "/**/*.shtml";
+var rootDirectory = "/Users/kotaronobuhara/code/KingOfPopMJ1.github.io"
+var inputDirectory = rootDirectory +"/inputFiles";
+var outputDirectory = rootDirectory;
+var matcher = "**/*.shtml";
  
+console.log("debug 1")
 var includes = new ssi(inputDirectory, outputDirectory, matcher);
+console.log("debug 2")
 includes.compile();
+console.log("debug 3")
